1acb366626e9ee190f1cbf0cca311533c338e1e3
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-09-17 15:53:23 +0900

    🔨 북마크 전체, 최근 학사일정 조회 Api 구현

--- ThinkerBell/data/src/main/java/com/neverland/data/datasourceImpl/BookmarkDataSourceImpl.kt
+++ ThinkerBell/data/src/main/java/com/neverland/data/datasourceImpl/BookmarkDataSourceImpl.kt
@@ -4,6 +4,8 @@ import com.neverland.data.datasource.BookmarkDataSource
 import com.neverland.data.remote.model.BaseResponse
 import com.neverland.data.remote.model.notice.BookmarkNoticeDTO
 import com.neverland.data.remote.model.notice.RecentBookmarkNoticeDTO
+import com.neverland.data.remote.model.univ.AcademicScheduleResDTO
+import com.neverland.data.remote.model.univ.RecentBookmarkScheduleResDTO
 import com.neverland.data.remote.service.BookmarkService
 import retrofit2.Response
 import javax.inject.Inject
@@ -15,8 +17,16 @@ class BookmarkDataSourceImpl @Inject constructor(
         return service.getNoticeBookmark(ssaId)
     }
 
+    override suspend fun getScheduleBookmark(ssaId: String): Response<BaseResponse<List<AcademicScheduleResDTO>>> {
+        return service.getScheduleBookmark(ssaId)
+    }
+
     override suspend fun getRecentNoticeBookmark(ssaId: String): Response<BaseResponse<List<RecentBookmarkNoticeDTO>>> {
         return service.getRecentNoticeBookmark(ssaId)
+    }
+
+    override suspend fun getRecentScheduleBookmark(ssaId: String): Response<BaseResponse<List<RecentBookmarkScheduleResDTO>>> {
+        return service.getRecentScheduleBookmark(ssaId)
     }
 
     override suspend fun postNoticeBookmark(
