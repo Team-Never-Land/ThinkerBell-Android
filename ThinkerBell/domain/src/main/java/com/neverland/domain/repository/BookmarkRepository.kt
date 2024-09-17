8813b469ee567697befaeac81239285d6a972c98
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-09-17 15:51:20 +0900

    🔨 북마크 전체, 최근 학사일정 조회 repository 구현

--- ThinkerBell/domain/src/main/java/com/neverland/domain/repository/BookmarkRepository.kt
+++ ThinkerBell/domain/src/main/java/com/neverland/domain/repository/BookmarkRepository.kt
@@ -2,12 +2,18 @@ package com.neverland.domain.repository
 
 import com.neverland.domain.model.notice.BookmarkNotice
 import com.neverland.domain.model.notice.RecentBookmarkNotice
+import com.neverland.domain.model.univ.AcademicSchedule
+import com.neverland.domain.model.univ.RecentAcademicSchedule
 
 interface BookmarkRepository {
 
     suspend fun getNoticeBookmark(ssaId: String): Result<BookmarkNotice>
 
+    suspend fun getScheduleBookmark(ssaId: String): Result<List<AcademicSchedule>>
+
     suspend fun getRecentNoticeBookmark(ssaId: String): Result<List<RecentBookmarkNotice>>
+
+    suspend fun getRecentScheduleBookmark(ssaId: String): Result<List<RecentAcademicSchedule>>
 
     suspend fun postNoticeBookmark(
         category: String,
